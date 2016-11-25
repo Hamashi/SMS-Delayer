e4598d9e42375386cc0fd2f25fd8d034c313b701
Hamashi <hamashii@gmail.com>
2016-11-25 11:40:27 +0100

    Write new activity's view is done, dont modify it anymor

--- app/src/main/java/gen/smsdelay/WriteNew.java
+++ app/src/main/java/gen/smsdelay/WriteNew.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 
 public class WriteNew extends AppCompatActivity {
@@ -15,5 +16,13 @@ public class WriteNew extends AppCompatActivity {
         setContentView(R.layout.activity_write_new);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    public boolean SendSMS(String number, String message)
+    {
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(number, null, message, null, null);
+
+        return true;
     }
 }
