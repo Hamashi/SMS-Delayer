ac0b4762fd51ca50a4ed2343bb6b753eeab7ea05
Iheb <ihebfehri17@gmail.com>
2016-12-19 15:05:37 +0100

    Cleaned WriteNew and sent the new push

--- app/src/main/java/gen/smsdelay/WriteNew.java
+++ app/src/main/java/gen/smsdelay/WriteNew.java
@@ -1,9 +1,11 @@
 package gen.smsdelay;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -17,6 +19,23 @@ import android.widget.EditText;
 import android.widget.TimePicker;
 import android.util.Log;
 import java.util.Date;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.icu.util.Calendar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.util.Log;
+import java.util.Date;
 
 import static android.app.PendingIntent.getService;
 
@@ -28,8 +47,6 @@ public class WriteNew extends AppCompatActivity {
     Button sendButton;
     EditText phoneText;
     EditText messageTest;
-    String phoneString;
-    String messageString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,28 +61,11 @@ public class WriteNew extends AppCompatActivity {
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                sendSMS();
+                SmsManager manager = SmsManager.getDefault();
+                manager.sendTextMessage(phoneText.getText().toString(), null, messageTest.getText().toString(), null, null);
             }
         });
     }
-
-    protected void sendSMS() {
-        phoneString = phoneText.getText().toString();
-        date = (DatePicker) findViewById(R.id.datePicker);
-        time = (TimePicker) findViewById(R.id.simpleTimePicker);
-
-        phoneString= phoneText.getText().toString();
-        messageString = messageTest.getText().toString();
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-
-                schedule(phoneString, messageString, date, time);
-            }
-        });
-
-    }
-
 
     public void schedule(String number, String message, DatePicker datePicker, TimePicker time)
     {
@@ -84,14 +84,13 @@ public class WriteNew extends AppCompatActivity {
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         //alarmManager.set(AlarmManager.RTC_WAKEUP, timestamp, getService(this, 0, , PendingIntent.FLAG_ONE_SHOT));
     }
+}
 
-    public boolean sendSMS(String number, String message)
-    {
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(number, null, message, null, null);
+    /* Old Code for sms
 
-        return true;
-    }
+    protected void sendSMS() {
+        phoneString = phoneText.getText().toString();
+        messageString = messageTest.getText().toString();
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
@@ -100,27 +99,30 @@ public class WriteNew extends AppCompatActivity {
             }
         }
     }
+     //inside SetOnclickListenenr
+                     //sendSMS();
+                Intent sms = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:"+phoneText.getText().toString()));
+                sms.putExtra("sms_body", messageTest.getText().toString());
+                startActivity(sms);
 
-        @Override
-        public void onRequestPermissionsResult(final int requestCode, String permissions[], final int[] grantResults) {
-            sendButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-            switch (requestCode) {
-                case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                    if ( grantResults.length > 0
-                            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        SmsManager smsManager = SmsManager.getDefault();
-                        smsManager.sendTextMessage(phoneString, null, messageString, null, null);
-                        Toast.makeText(getApplicationContext(), "SMS sent.",
-                                Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getApplicationContext(),
-                                "SMS failed, please try again.", Toast.LENGTH_LONG).show();
-                        return;
-                    }
+    @Override
+    public void onRequestPermissionsResult(final int requestCode,String permissions[],final int[] grantResults) {
+        if (requestCode) {
+             MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneText.getText().toString(), null, messageTest.getText().toString(), null, null);
+                    Toast.makeText(getApplicationContext(), "SMS sent.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS failed, please try again.", Toast.LENGTH_LONG).show();
+                    return;
                 }
             }
-            }
-        });
+        }
     }
-}
+
+     */
+
+
