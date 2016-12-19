7f641424780ce87b38cbadbacf05f5dd22e2aa2e
Hamashi <hamashii@gmail.com>
2016-12-19 17:38:23 +0100

    Notification when the SMS is sent

--- app/src/main/java/gen/smsdelay/SendSMS.java
+++ app/src/main/java/gen/smsdelay/SendSMS.java
@@ -1,10 +1,13 @@
 package gen.smsdelay;
 
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsManager;
-
 
 public class SendSMS extends BroadcastReceiver {
 
@@ -14,8 +17,26 @@ public class SendSMS extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         SmsManager manager = SmsManager.getDefault();
-        manager.sendTextMessage(NUMBER, null, MESSAGE, null, null);
+        String message = intent.getParcelableExtra(MESSAGE);
+        String number = intent.getParcelableExtra(NUMBER);
+
+        manager.sendTextMessage(number, null, message, null, null);
+
+        notif(context);
 
     }
+    public void notif(Context context){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.mipmap.delaysms_launcher)
+                        .setContentTitle("SMS Delayer")
+                        .setContentText("SMS Sent");
+
+        NotificationManager mNotifyMgr =
+                (NotificationManager)  context.getSystemService(context.NOTIFICATION_SERVICE);
+
+        mNotifyMgr.notify(1, mBuilder.build());
+    }
+
 
 }
